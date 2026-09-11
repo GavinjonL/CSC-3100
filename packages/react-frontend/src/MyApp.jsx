9a963450df4c981e75bfc907d80e7690eac302ea
GavinjonL <bigbraingavin@gmail.com>
2026-09-10 19:38:25 -0700

    Created Form.jsx and updated MyApp.jsx

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,24 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    }, 
-    {
-        name: "Mac",
-        job: "Bouncer",
-    },
-    {
-        name: "Dee",
-        job: "Aspring actress",
-    },
-    {
-        name: "Dennis",
-        job: "Bartender",
-    }
   ]);
 
   function removeOneCharacter(index) {
@@ -29,9 +14,10 @@ function MyApp() {
   }
 
   return (
-    <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-    </div>
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form />
+  </div>
 );
 }
 
